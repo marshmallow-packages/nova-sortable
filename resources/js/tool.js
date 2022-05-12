7ef79890ef56e399bea79e2094cc1c1f2f9e3fa2
LTKort <lars.kort94@gmail.com>
2022-05-12 13:52:26 +0200

    Component Import

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,17 +1,9 @@
-// import ResourceTable from './components/ResourceTable';
-// import ResourceTableRow from './components/ResourceTableRow';
-// import ReorderButtons from './components/ReorderButtons';
+import ResourceTable from './components/ResourceTable';
+import ResourceTableRow from './components/ResourceTableRow';
+import ReorderButtons from './components/ReorderButtons';
 
-// Nova.booting((app, router, store) => {
-//   app.component('resource-table', ResourceTable);
-//   app.component('resource-table-row', ResourceTableRow);
-//   app.component('reorder-buttons', ReorderButtons);
-// });
-
-const Vue = require('@vue/compat/dist/vue.cjs.prod');
-
-Nova.booting(Vue => {
-  Vue.component('ResourceTable', require('./components/ResourceTable').default);
-  Vue.component('ResourceTableRow', require('./components/ResourceTableRow').default);
-  Vue.component('ReorderButtons', require('./components/ReorderButtons').default);
+Nova.booting((app, router, store) => {
+  app.component('ResourceTable', require('./components/ResourceTable').default);
+  app.component('ResourceTableRow', require('./components/ResourceTableRow').default);
+  app.component('ReorderButtons', require('./components/ReorderButtons').default);
 });
