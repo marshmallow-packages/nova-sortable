06026fe144056ff85957e7b31045a073c85491ac
LTKort <lars.kort94@gmail.com>
2023-12-20 11:40:08 +0100

    header

--- resources/js/components/ResourceTableHeader.vue
+++ resources/js/components/ResourceTableHeader.vue
@@ -3,24 +3,25 @@
     <tr>
       <!-- Select Checkbox -->
       <th
-        class="td-fit mm-uppercase text-xxs text-gray-500 mm-tracking-wide mm-pl-5 mm-pr-2 mm-py-2"
+        class="td-fit uppercase text-xxs text-gray-500 tracking-wide pl-5 pr-2 py-2"
         :class="{
-          'mm-border-r border-gray-200 dark:border-gray-600': shouldShowColumnBorders,
+          'border-r border-gray-200 dark:border-gray-600': shouldShowColumnBorders,
         }"
         v-if="shouldShowCheckboxes || canSeeReorderButtons"
       >
-        <span v-if="shouldShowCheckboxes" class="mm-sr-only">{{ __('Selected Resources') }}</span>
+        <span v-if="shouldShowCheckboxes" class="sr-only">{{ __('Selected Resources') }}</span>
       </th>
 
+      <!-- Field Names -->
       <th
         v-for="(field, index) in fields"
         :key="field.uniqueKey"
         :class="{
           [`text-${field.textAlign}`]: true,
-          'mm-border-r border-gray-200 dark:border-gray-600': shouldShowColumnBorders,
-          'mm-px-6': index == 0 && !shouldShowCheckboxes && !canSeeReorderButtons,
-          'mm-px-2': index != 0 || shouldShowCheckboxes || canSeeReorderButtons,
-          'mm-whitespace-nowrap': !field.wrapping,
+          'border-r border-gray-200 dark:border-gray-600': shouldShowColumnBorders,
+          'px-6': index == 0 && !shouldShowCheckboxes && !canSeeReorderButtons,
+          'px-2': index != 0 || shouldShowCheckboxes || canSeeReorderButtons,
+          'whitespace-nowrap': !field.wrapping,
         }"
         class="mm-uppercase text-gray-500 text-xxs mm-tracking-wide mm-py-2"
       >
